4ec70f3dd3bf81a96fe9c92f133c8be028aa4fdf
Hari Chalise <ingoprivate@gmail.com>
2025-02-25 01:18:23 +0545

    Astro Starlight [ Docs Test 🌟 ]
    Co-Authored-By: aayush <177418358+aayushrg7@users.noreply.github.com>

---
+++ astro.config.mjs
@@ -0,0 +1,38 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+import starlight from '@astrojs/starlight';
+
+// https://astro.build/config
+export default defineConfig({
+  site: 'https://carch-org.io',
+  base: '/wiki',
+	integrations: [
+		starlight({
+			title: 'Carch',
+			social: {
+        discord: 'https://discord.com/invite/8NJWstnUHd',
+        reddit: 'https://www.reddit.com/r/carch/',
+        telegram: 'https://t.me/carchx'
+			},
+      editLink: {
+          baseUrl: 'https://github.com/carch-org/wiki/tree/main',
+      },
+      customCss: ['./src/styles/custom.css'],
+      tableOfContents: { minHeadingLevel: 1, maxHeadingLevel: 2, },
+      favicon: './carch.png',
+      lastUpdated: true,
+			sidebar: [
+				{ label: 'Introduction', slug: 'getting-started/introduction' },
+				{ label: 'Installation', slug: 'getting-started/installation' },
+				{ label: 'Commands', slug: 'reference/commands' },
+				{ label: 'Overview Scripts', slug: 'reference/overview-scripts' },
+				{ label: 'Roadmap', slug: 'project/roadmap' },
+				{ label: 'Contributing', slug: 'project/contributing' },
+				{ label: 'Code of Conduct', slug: 'project/code-of-conduct' },
+				{ label: 'Inspiration', slug: 'project/inspiration' },
+				{ label: 'Contributions', slug: 'project/contributions' },
+			],
+		}),
+	],
+});
+
