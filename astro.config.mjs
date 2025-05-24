2475366c0059442e84886ad30ee350700d0fe041
Hari Chalise <ingoprivate@gmail.com>
2025-05-25 04:00:47 +0545

    fix: light mode color

--- astro.config.mjs
+++ astro.config.mjs
@@ -13,7 +13,7 @@ export default defineConfig({
       plugins: [
         catppuccin({
           dark: { flavor: "mocha", accent: "flamingo" },
-          light: { flavor: "latte", accent: "flamingo" },
+          light: { flavor: "latte", accent: "maroon" },
         }),
       ],
       social: [
